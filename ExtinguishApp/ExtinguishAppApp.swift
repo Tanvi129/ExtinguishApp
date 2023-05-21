993c5074ac08a20a2e3a7f977898b9111417021e
Tanvi129 <tanvigupta129@gmail.com>
2023-05-21 21:11:46 +0530

    Initial Commit

---
+++ ExtinguishApp/ExtinguishAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExtinguishAppApp.swift
+//  ExtinguishApp
+//
+//  Created by Tanvi Gupta on 21/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ExtinguishAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
