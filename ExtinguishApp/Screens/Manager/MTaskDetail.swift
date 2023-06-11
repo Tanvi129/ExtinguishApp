684e5e872ec63e6ae51ef25344744810cb92bca8
Tanvi129 <tanvigupta129@gmail.com>
2023-06-11 14:41:54 +0530

    commit

---
+++ ExtinguishApp/Screens/Manager/MTaskDetail.swift
@@ -0,0 +1,69 @@
+//
+//  MTaskDetail.swift
+//  ExtinguishApp
+//
+//  Created by Tanvi Gupta on 11/06/23.
+//
+
+import SwiftUI
+
+struct MTaskDetail: View {
+    var body: some View {
+        VStack(alignment: .leading){
+            VStack{
+                Image("MProfile")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 100, height: 100)
+                    .clipShape(Circle())
+                Text("Stock Destroy").font(.title.bold())
+                Text("XXa | Cipla").font(.title2.weight(.semibold))
+                HStack(spacing : 10){
+                    Text("Assigned to - Mr Vansh Agarwal").font(.title3)
+                    Image(systemName: "phone.fill")
+                        
+                        .background( Circle().fill(Color("Primary")).frame(width: 30 , height: 30))
+                }
+            }.frame(maxWidth: .infinity)
+                
+            Divider().padding(.vertical , 10)
+            ScrollView(.vertical ){
+                VStack(alignment : .leading, spacing: 10){
+                    HStack(alignment : . center , spacing:  20){
+                        Image(systemName: "clock.fill")
+                        VStack(alignment : .leading){
+                            Text("Sat")
+                            Text("9:00 AM - 10:00 AM")
+                        }
+                    }
+                    HStack(alignment : . center , spacing:  20){
+                        Image(systemName: "mappin.and.ellipse")
+                        Text("Cipla , Chennai")
+                    }
+                    HStack {
+                        VStack(alignment : .leading){
+                            Text("Contact Person Name").font(.title2.weight(.semibold))
+                            Text("Tanvi Gupta").font(.title3)
+                        }
+                        Spacer()
+                        Image(systemName: "phone.fill").frame( width : 50, height: 50)
+                    }.padding()
+                        .frame(maxWidth: .infinity , alignment: .leading).background(Color("Primary"))
+                    .cornerRadius(15)
+                    
+                    
+                }
+                
+                
+                
+            }
+        }
+        .padding(24)
+    }
+}
+
+struct MTaskDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        MTaskDetail()
+    }
+}
